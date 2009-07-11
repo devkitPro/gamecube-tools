266a78333c93447635029c2134b6b2b6b38bbc5b
Dave Murphy <wntrmute@users.sourceforge.net>
2009-07-11 00:45:11 +0000

    bump version

--- source/gxtexconv.cpp
+++ source/gxtexconv.cpp
@@ -8,7 +8,7 @@
 
 static void usage()
 {
-	fprintf(stderr, "gxtexconv v0.1.6\n");
+	fprintf(stderr, "gxtexconv v0.1.7\n");
 	fprintf(stderr, "coded by shagkur, #wiidev blitzed\n");
 	fprintf(stderr, "powered by FreeImage library\n\n");
 	fprintf(stderr, "usage: gxtexconv -i <imagepath> [-o <outputfile>.tpl colfmt=<texfmt> mipmap=yes minlod=<min level> maxlod=<max level> width=<newwidth> height=<newheight> [palfmt=<palcolfmt>]]\n");
